dfda64886a9e296c6b9e1af625bdbd41fb24d498
officialbubies <sebastianbuberry@gmail.com>
2022-10-04 15:07:51 -0500

    added LIMIT to part A, removed COUNT on part B

--- scripts/question_2.sql
+++ scripts/question_2.sql
@@ -1,24 +1,14 @@
-/*
-	2. Find the name and height of the shortest player in the database. 
-		- Eddie Gaedel is shortest player in DB at 43cm?
-		
-	   How many games did he play in? 
-	    - 52? or 1?
-		
-	   What is the name of the team for which he played?
-	    - St. Louis Browns
-*/
-
 -- find shortest player --
 SELECT namefirst, 
 	   namelast, 
 	   MIN(height) AS short_player, playerid
 FROM people
 GROUP BY namefirst, namelast, height, playerid
-ORDER BY height;
+ORDER BY height
+LIMIT 1;
 
 -- how many games Eddie played in AND what team name --
-SELECT COUNT(G_all), 
+SELECT G_all, 
 	   teamID, 
 	   t.name, 
 	   a.yearID
@@ -26,4 +16,4 @@ FROM appearances AS a
 INNER JOIN teams AS t
 USING(teamID)
 WHERE playerid = 'gaedeed01'
-GROUP BY a.teamID, t.name, a.yearID;
+GROUP BY a.teamID, t.name, a.yearID, a.G_all;
