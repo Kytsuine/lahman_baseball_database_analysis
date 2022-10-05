0c864a849343b7855e196c23a1d0ea11c60c829b
officialbubies <sebastianbuberry@gmail.com>
2022-10-05 16:50:14 -0400

    create branch for q10, empty sql file

---
+++ scripts/question_10.sql
@@ -0,0 +1,6 @@
+/*
+	10. Find all players who hit their career highest number of home runs in 2016. 
+		Consider only players who have played in the league for at least 10 years, and who hit at least one home run in 2016. 
+		Report the players' first and last names and the number of home runs they hit in 2016.
+*/
+
