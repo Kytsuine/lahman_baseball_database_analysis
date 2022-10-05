37cf6c2a01e2917c19e26bf667081b78de790a14
officialbubies <sebastianbuberry@gmail.com>
2022-10-05 11:44:44 -0500

    avg so and hr by decade v1

--- scripts/question_5.sql
+++ scripts/question_5.sql
@@ -5,8 +5,13 @@
 */
 
 -- average num strikeouts per game since 1920 --
-SELECT DISTINCT yearid, ROUND(AVG(p.SO) OVER(PARTITION BY yearid),2) AS avg_so
+
+SELECT 10*(yearid / 10) as decade,
+	   ROUND(AVG(p.SO),2) AS avg_so,
+	   ROUND(AVG(b.HR),2) AS avg_hr
 FROM pitching AS p
-INNER JOIN appearances as a
-USING(yearid)
-GROUP BY yearid, p.so
+INNER JOIN batting as b
+	USING(yearid)
+WHERE yearid > 1920
+GROUP BY decade;
+ORDER BY decade
