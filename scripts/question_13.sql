ff23b432870fbf323cb537b0b8fd9cb8fed27c65
Lewis-Garvin <lewisgarvin@gmail.com>
2022-10-05 16:25:33 -0500

    Creating branch and script file

---
+++ scripts/question_13.sql
@@ -0,0 +1,5 @@
+--13. It is thought that since left-handed pitchers are more rare, causing batters to face 
+--    them less often, that they are more effective. Investigate this claim and present 
+--    evidence to either support or dispute this claim. First, determine just how rare left-handed 
+--    pitchers are compared with right-handed pitchers. Are left-handed pitchers more likely to win 
+--    the Cy Young Award? Are they more likely to make it into the hall of fame?
